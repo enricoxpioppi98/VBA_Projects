7691a38b3988310d2d04e6a559b78a771a11100c
enricoxpioppi98 <83033484+enricoxpioppi98@users.noreply.github.com>
2023-03-08 22:57:27 -0500

    Update BaselineBOM.vb
    Better formatting and more efficient.

--- Update Baseline BOM/BaselineBOM.vb
+++ Update Baseline BOM/BaselineBOM.vb
@@ -8,54 +8,80 @@ Public Function IsInArray(ToBeFound As Variant, arr As Variant, NumberOfElements
         End If
     Next i
 End Function
-Sub BaselineBOM()
+Sub Baseline_BOM()
 
-    Application.ScreenUpdating = False
-    Application.Calculation = xlCalculationManual
-    
-    xCurrentRow = ActiveCell.Row
-    xCurrentColumn = ActiveCell.Column
-    
-    Range("A3:F143361").Font.Color = RGB(0, 0, 0)
-    Range("A3:F143361").Font.Name = "Arial"
-    Range("A3:F143361").Font.Size = 12
-    
-    Range("A3:D3").Interior.Color = RGB(242, 242, 242)
-    Range("A4:D4").Interior.Color = RGB(217, 217, 217)
-    Range("E3:F4").Interior.Color = RGB(255, 255, 153)
-    
-    Range("A3:A4,C3:C4").Select
-    Selection.Borders(xlDiagonalDown).LineStyle = xlNone
-    Selection.Borders(xlDiagonalUp).LineStyle = xlNone
-    Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeTop).LineStyle = xlNone
-    Selection.Borders(xlEdgeBottom).LineStyle = xlNone
-    Selection.Borders(xlEdgeRight).LineStyle = xlNone
-    Selection.Borders(xlInsideVertical).LineStyle = xlNone
-    Selection.Borders(xlInsideHorizontal).LineStyle = xlNone
-    
-    Range("E3:F4").Select
-    Selection.Borders(xlDiagonalDown).LineStyle = xlNone
-    Selection.Borders(xlDiagonalUp).LineStyle = xlNone
-    Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeTop).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeBottom).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeRight).LineStyle = xlContinuous
-    Selection.Borders(xlInsideVertical).LineStyle = xlContinuous
-    Selection.Borders(xlInsideHorizontal).LineStyle = xlContinuous
-    
-    Range("A3:F4").Copy
-    Range("A5:F143361").PasteSpecial Paste:=xlPasteFormats
-    Application.CutCopyMode = False
+    'Improve Execution ------------------------------------------------------------------------------------------------------------------------------
+        Application.ScreenUpdating = False
+        Application.Calculation = xlCalculationManual
 
-    'Count parts
+        Current_Row = ActiveCell.Row
+        Current_Column = ActiveCell.Column
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+
+    'Initial Formatting -----------------------------------------------------------------------------------------------------------------------------
+        'BOM ----------------------------------------------------------------------------------------------------------------------------------------
+            With Range("A3:B4")
+                'Borders ----------------------------------------------------------------------------------------------------------------------------
+                    .Borders(xlDiagonalDown).LineStyle = xlNone
+                    .Borders(xlDiagonalUp).LineStyle = xlNone
+                    .Borders(xlEdgeTop).LineStyle = xlNone
+                    .Borders(xlEdgeBottom).LineStyle = xlNone
+                    .Borders(xlInsideVertical).LineStyle = xlNone
+                    .Borders(xlInsideHorizontal).LineStyle = xlNone
+                    .Borders(xlEdgeLeft).LineStyle = xlContinuous
+                    .Borders(xlEdgeRight).LineStyle = xlContinuous
+                '------------------------------------------------------------------------------------------------------------------------------------
+                'Font -------------------------------------------------------------------------------------------------------------------------------
+                    .Font.Name = "Arial"
+                    .Font.Size = 12
+                    .Font.Color = RGB(0, 0, 0)
+                    .Font.Bold = False
+                '------------------------------------------------------------------------------------------------------------------------------------
+            End With
+            Range("A3:B3").Interior.Color = RGB(242, 242, 242)
+            Range("A4:B4").Interior.Color = RGB(217, 217, 217)
+        '--------------------------------------------------------------------------------------------------------------------------------------------
+        'QIS ----------------------------------------------------------------------------------------------------------------------------------------
+            Range("A3:B4").Copy
+            Range("A3:D512").PasteSpecial Paste:=xlPasteFormats
+        '--------------------------------------------------------------------------------------------------------------------------------------------
+        'Final BOM ----------------------------------------------------------------------------------------------------------------------------------
+            With Range("E3:F512")
+                .Interior.Color = RGB(255, 255, 153)
+                'Borders ----------------------------------------------------------------------------------------------------------------------------
+                    .Borders(xlDiagonalDown).LineStyle = xlNone
+                    .Borders(xlDiagonalUp).LineStyle = xlNone
+                    .Borders(xlEdgeTop).LineStyle = xlContinuous
+                    .Borders(xlEdgeBottom).LineStyle = xlContinuous
+                    .Borders(xlInsideVertical).LineStyle = xlContinuous
+                    .Borders(xlInsideHorizontal).LineStyle = xlContinuous
+                    .Borders(xlEdgeLeft).LineStyle = xlContinuous
+                    .Borders(xlEdgeRight).LineStyle = xlContinuous
+                '------------------------------------------------------------------------------------------------------------------------------------
+                'Font -------------------------------------------------------------------------------------------------------------------------------
+                    .Font.Name = "Arial"
+                    .Font.Size = 12
+                    .Font.Color = RGB(255, 0, 0)
+                    .Font.Bold = False
+                '------------------------------------------------------------------------------------------------------------------------------------
+                .HorizontalAlignment = xlLeft
+            End With
+            With Range("B3:B512, D3:D512, F3:F512")
+                .NumberFormat = "0.0000"
+                .HorizontalAlignment = xlRight
+            End With
+        '--------------------------------------------------------------------------------------------------------------------------------------------
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+
+    'Count Parts ------------------------------------------------------------------------------------------------------------------------------------
         QIS_LastRow = Cells(Rows.Count, 1).End(xlUp).Row
         QIS_NumberOfParts = QIS_LastRow - 2
         
         CM_LastRow = Cells(Rows.Count, 3).End(xlUp).Row
         CM_NumberOfParts = CM_LastRow - 2
-        
-    'arrays
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+
+    'Arrays -----------------------------------------------------------------------------------------------------------------------------------------
         Dim QIS() As Variant
         ReDim QIS(1 To QIS_NumberOfParts, 1 To 2)
         For Row = 3 To QIS_LastRow
@@ -71,8 +97,10 @@ Sub BaselineBOM()
             CM(i, 1) = Cells(Row, 3).Value
             CM(i, 2) = Cells(Row, 4).Value
         Next Row
-        
-    'final
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+
+    'Calculate Final BOM ----------------------------------------------------------------------------------------------------------------------------
+
         For i = 1 To CM_NumberOfParts
             Row = i + 2
             Cells(Row, 5).Value = CM(i, 1)                                          'paste CM BOM
@@ -80,76 +108,51 @@ Sub BaselineBOM()
             If QIS_Element <> "" Then                                               'If CMitem is in QIS
                 Cells(Row, 6).Value = QIS(QIS_Element, 2)                           'Paste QIS quantity and leave QISitem as 0
                 If QIS(QIS_Element, 2) <> CM(i, 2) Then                             'If quantity changed
-                    With Cells(Row, 5)
-                        .Interior.Color = RGB(255, 255, 0)
-                        .Font.Color = RGB(255, 0, 0)
-                        .Font.Bold = True
-                        .HorizontalAlignment = xlRight
-                        .Font.Strikethrough = False
-                    End With
-                    With Cells(Row, 6)
-                        .Interior.Color = RGB(255, 255, 0)
-                        .Font.Color = RGB(255, 0, 0)
-                        .Font.Bold = True
-                        .HorizontalAlignment = xlRight
-                        .Font.Strikethrough = False
-                        .NumberFormat = "0.0000"
-                    End With
-                Else                                                                 'If NO change
-                    With Cells(Row, 5)
-                        .Interior.Color = RGB(255, 255, 153)
-                        .Font.Color = RGB(255, 0, 0)
-                        .HorizontalAlignment = xlLeft
-                        .Font.Bold = False
-                        .Font.Strikethrough = False
-                    End With
-                    With Cells(Row, 6)
-                        .Interior.Color = RGB(255, 255, 153)
-                        .Font.Color = RGB(255, 0, 0)
-                        .HorizontalAlignment = xlRight
-                        .Font.Bold = False
-                        .Font.Strikethrough = False
-                        .NumberFormat = "0.0000"
-                    End With
+                    'Bold, Align Right, Highlight Yellow  -------------------------------------------------------------------------------------------
+                        With Range("E" & Row & ":F" & Row)
+                            .Font.Bold = True
+                            .HorizontalAlignment = xlRight
+                            .Interior.Color = RGB(255, 255, 0)
+                        End With
+                    '--------------------------------------------------------------------------------------------------------------------------------
                 End If
-            Else                                                                    'If CMitem is NOT in QIS (=removed)
+            Else                                                                    'If CM item is NOT in QIS (=removed)
                 Cells(Row, 6).Value = ""                                            'Leave quantity blank
-                With Cells(Row, 5)                                                  'Highlight green + strikethrough
-                    .Interior.Color = RGB(226, 239, 218)
-                    .Font.Color = RGB(255, 0, 0)
-                    .Font.Bold = True
-                    .HorizontalAlignment = xlRight
-                    .Font.Strikethrough = True
-                End With
+                'Bold, Align Right, Highlight Green, Strikethrough  ---------------------------------------------------------------------------------
+                    With Range("E" & Row & ":F" & Row)
+                        .Font.Bold = True
+                        .HorizontalAlignment = xlRight
+                        .Interior.Color = RGB(226, 239, 218)
+                        .Font.Strikethrough = True
+                    End With
+                '------------------------------------------------------------------------------------------------------------------------------------
             End If
             Final_LastRow = Row
         Next i
+
         For i = 1 To QIS_NumberOfParts
             If QIS(i, 1) <> 0 Then                                                  'If QISitem is NOT 0 (= is NOT in CM)
                 Final_LastRow = Final_LastRow + 1
+
                 Cells(Final_LastRow, 5).Value = QIS(i, 1)                           'Paste QISitem
                 Cells(Final_LastRow, 6).Value = QIS(i, 2)                           'Paste QISitem quantity
-                With Cells(Final_LastRow, 5)
-                    .Interior.Color = RGB(255, 255, 0)
-                    .Font.Color = RGB(255, 0, 0)
-                    .Font.Bold = True
-                    .HorizontalAlignment = xlRight
-                    .Font.Strikethrough = False
-                End With
-                With Cells(Final_LastRow, 6)
-                    .Interior.Color = RGB(255, 255, 0)
-                    .Font.Color = RGB(255, 0, 0)
-                    .Font.Bold = True
-                    .HorizontalAlignment = xlRight
-                    .Font.Strikethrough = False
-                    .NumberFormat = "0.0000"
-                End With
+
+                'Bold, Align Right, Highlight Yellow  -------------------------------------------------------------------------------------------
+                    With Range("E" & Final_LastRow & ":F" & Final_LastRow)
+                        .Font.Bold = True
+                        .HorizontalAlignment = xlRight
+                        .Interior.Color = RGB(255, 255, 0)
+                    End With
+                '--------------------------------------------------------------------------------------------------------------------------------
             End If
         Next i
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+
+    'Restore Execution ------------------------------------------------------------------------------------------------------------------------------
+        Cells(Current_Row, Current_Column).Select
         
-        Cells(xCurrentRow, xCurrentColumn).Select
-        
-    Application.ScreenUpdating = True
-    Application.Calculation = xlCalculationAutomatic
-        
+        Application.ScreenUpdating = True
+        Application.Calculation = xlCalculationAutomatic
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+    
 End Sub
