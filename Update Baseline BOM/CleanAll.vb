ceae3e6867d1d1a84606d070ce1b08b1f40966dc
enricoxpioppi98 <83033484+enricoxpioppi98@users.noreply.github.com>
2023-03-08 23:03:40 -0500

    Update CleanAll.vb

--- Update Baseline BOM/CleanAll.vb
+++ Update Baseline BOM/CleanAll.vb
@@ -1,48 +1,75 @@
 Sub CleanAll()
 
-    Application.ScreenUpdating = False
-    Application.Calculation = xlCalculationManual
-    
-    xCurrentRow = ActiveCell.Row
-    xCurrentColumn = ActiveCell.Column
-    
-    Range("A3:F143361").ClearContents
-    
-    Range("A3:F143361").Font.Color = RGB(0, 0, 0)
-    Range("A3:F143361").Font.Name = "Arial"
-    Range("A3:F143361").Font.Size = 12
-    
-    Range("A3:D3").Interior.Color = RGB(242, 242, 242)
-    Range("A4:D4").Interior.Color = RGB(217, 217, 217)
-    Range("E3:F4").Interior.Color = RGB(255, 255, 153)
-    
-    Range("A3:A4,C3:C4").Select
-    Selection.Borders(xlDiagonalDown).LineStyle = xlNone
-    Selection.Borders(xlDiagonalUp).LineStyle = xlNone
-    Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeTop).LineStyle = xlNone
-    Selection.Borders(xlEdgeBottom).LineStyle = xlNone
-    Selection.Borders(xlEdgeRight).LineStyle = xlNone
-    Selection.Borders(xlInsideVertical).LineStyle = xlNone
-    Selection.Borders(xlInsideHorizontal).LineStyle = xlNone
-    
-    Range("E3:F4").Select
-    Selection.Borders(xlDiagonalDown).LineStyle = xlNone
-    Selection.Borders(xlDiagonalUp).LineStyle = xlNone
-    Selection.Borders(xlEdgeLeft).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeTop).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeBottom).LineStyle = xlContinuous
-    Selection.Borders(xlEdgeRight).LineStyle = xlContinuous
-    Selection.Borders(xlInsideVertical).LineStyle = xlContinuous
-    Selection.Borders(xlInsideHorizontal).LineStyle = xlContinuous
-    
-    Range("A3:F4").Copy
-    Range("A5:F143361").PasteSpecial Paste:=xlPasteFormats
-    Application.CutCopyMode = False
-    
-    Cells(xCurrentRow, xCurrentColumn).Select
-    
-    Application.ScreenUpdating = True
-    Application.Calculation = xlCalculationAutomatic
+    'Improve Execution ------------------------------------------------------------------------------------------------------------------------------
+        Application.ScreenUpdating = False
+        Application.Calculation = xlCalculationManual
+
+        Current_Row = ActiveCell.Row
+        Current_Column = ActiveCell.Column
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+
+    Range("A3:F512").ClearContents
+
+    'Initial Formatting -----------------------------------------------------------------------------------------------------------------------------
+        'BOM ----------------------------------------------------------------------------------------------------------------------------------------
+            With Range("A3:B4")
+                'Borders ----------------------------------------------------------------------------------------------------------------------------
+                    .Borders(xlDiagonalDown).LineStyle = xlNone
+                    .Borders(xlDiagonalUp).LineStyle = xlNone
+                    .Borders(xlEdgeTop).LineStyle = xlNone
+                    .Borders(xlEdgeBottom).LineStyle = xlNone
+                    .Borders(xlInsideVertical).LineStyle = xlNone
+                    .Borders(xlInsideHorizontal).LineStyle = xlNone
+                    .Borders(xlEdgeLeft).LineStyle = xlContinuous
+                    .Borders(xlEdgeRight).LineStyle = xlContinuous
+                '------------------------------------------------------------------------------------------------------------------------------------
+                'Font -------------------------------------------------------------------------------------------------------------------------------
+                    .Font.Name = "Arial"
+                    .Font.Size = 12
+                    .Font.Color = RGB(0, 0, 0)
+                    .Font.Bold = False
+                '------------------------------------------------------------------------------------------------------------------------------------
+            End With
+            Range("A3:B3").Interior.Color = RGB(242, 242, 242)
+            Range("A4:B4").Interior.Color = RGB(217, 217, 217)
+        '--------------------------------------------------------------------------------------------------------------------------------------------
+        'QIS ----------------------------------------------------------------------------------------------------------------------------------------
+            Range("A3:B4").Copy
+            Range("A3:D512").PasteSpecial Paste:=xlPasteFormats
+        '--------------------------------------------------------------------------------------------------------------------------------------------
+        'Final BOM ----------------------------------------------------------------------------------------------------------------------------------
+            With Range("E3:F512")
+                .Interior.Color = RGB(255, 255, 153)
+                'Borders ----------------------------------------------------------------------------------------------------------------------------
+                    .Borders(xlDiagonalDown).LineStyle = xlNone
+                    .Borders(xlDiagonalUp).LineStyle = xlNone
+                    .Borders(xlEdgeTop).LineStyle = xlContinuous
+                    .Borders(xlEdgeBottom).LineStyle = xlContinuous
+                    .Borders(xlInsideVertical).LineStyle = xlContinuous
+                    .Borders(xlInsideHorizontal).LineStyle = xlContinuous
+                    .Borders(xlEdgeLeft).LineStyle = xlContinuous
+                    .Borders(xlEdgeRight).LineStyle = xlContinuous
+                '------------------------------------------------------------------------------------------------------------------------------------
+                'Font -------------------------------------------------------------------------------------------------------------------------------
+                    .Font.Name = "Arial"
+                    .Font.Size = 12
+                    .Font.Color = RGB(255, 0, 0)
+                    .Font.Bold = False
+                '------------------------------------------------------------------------------------------------------------------------------------
+                .HorizontalAlignment = xlLeft
+            End With
+            With Range("B3:B512, D3:D512, F3:F512")
+                .NumberFormat = "0.0000"
+                .HorizontalAlignment = xlRight
+            End With
+        '--------------------------------------------------------------------------------------------------------------------------------------------
+    '------------------------------------------------------------------------------------------------------------------------------------------------
+
+    'Restore Execution ------------------------------------------------------------------------------------------------------------------------------
+        Cells(Current_Row, Current_Column).Select
+        
+        Application.ScreenUpdating = True
+        Application.Calculation = xlCalculationAutomatic
+    '------------------------------------------------------------------------------------------------------------------------------------------------
     
 End Sub
